a1fde594137cc77ac3348a94a80739b550647168
Yasser Rachid <yrachid@thoughtworks.com>
2018-10-16 22:50:25 -0300

    Passo01: Livros em memoria

--- app/src/main/java/com/thoughtworks/aceleradora/biblioteca/BibliotecaController.java
+++ app/src/main/java/com/thoughtworks/aceleradora/biblioteca/BibliotecaController.java
@@ -8,7 +8,14 @@ public class BibliotecaController {
 
     @GetMapping("/")
     public String listaDeLivros() {
-       return "livros/lista";
+
+        Livro oitentaEQuatro = new Livro("1984");
+        Livro admiravelMundoNovo = new Livro("Admiravel Mundo Novo");
+        Livro policarpoQuaresma = new Livro("Triste fim de Policarpo Quaresma");
+        Livro fahrenheit451 = new Livro("Fahrenheit 451");
+        Livro maus = new Livro("Maus");
+
+        return "livros/lista";
     }
 
 }
