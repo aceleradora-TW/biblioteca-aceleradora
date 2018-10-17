a7f9d768041a839bda514266b288a1967f118657
Yasser Rachid <yrachid@thoughtworks.com>
2018-10-16 23:15:53 -0300

    Passo01: Lista de livros na view

--- app/src/main/java/com/thoughtworks/aceleradora/biblioteca/BibliotecaController.java
+++ app/src/main/java/com/thoughtworks/aceleradora/biblioteca/BibliotecaController.java
@@ -1,19 +1,33 @@
 package com.thoughtworks.aceleradora.biblioteca;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class BibliotecaController {
 
     @GetMapping("/")
-    public String listaDeLivros() {
+    public String listaDeLivros(Model model) {
 
         Livro oitentaEQuatro = new Livro("1984");
         Livro admiravelMundoNovo = new Livro("Admiravel Mundo Novo");
         Livro policarpoQuaresma = new Livro("Triste fim de Policarpo Quaresma");
         Livro fahrenheit451 = new Livro("Fahrenheit 451");
         Livro maus = new Livro("Maus");
+
+        List<Livro> livros = Arrays.asList(
+                oitentaEQuatro,
+                admiravelMundoNovo,
+                policarpoQuaresma,
+                fahrenheit451,
+                maus
+        );
+
+        model.addAttribute("livros", livros);
 
         return "livros/lista";
     }
