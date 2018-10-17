2be11f5bbf6bf9a2e614c8154416d89bb186344f
Yasser Rachid <yrachid@thoughtworks.com>
2018-10-16 22:49:17 -0300

    Passo01: A classe Livro

---
+++ app/src/main/java/com/thoughtworks/aceleradora/biblioteca/Livro.java
@@ -0,0 +1,28 @@
+package com.thoughtworks.aceleradora.biblioteca;
+
+public class Livro {
+
+    private String titulo;
+
+    public Livro() {
+    }
+
+    public Livro(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "titulo='" + titulo + '\'' +
+                '}';
+    }
+}
