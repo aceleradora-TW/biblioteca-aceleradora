96f149e5a6d57077aa7e4e4397506dd21367badd
othman853 <anuar.yasser@gmail.com>
2018-10-17 15:18:24 -0300

    Configura banco de dados

--- app/src/main/java/com/thoughtworks/aceleradora/biblioteca/BibliotecaController.java
+++ app/src/main/java/com/thoughtworks/aceleradora/biblioteca/BibliotecaController.java
@@ -3,6 +3,7 @@ package com.thoughtworks.aceleradora.biblioteca;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -10,6 +11,22 @@ import static java.util.Arrays.asList;
 
 @Controller
 public class BibliotecaController {
+
+    LivroRepository repository;
+
+    public BibliotecaController(LivroRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/cria")
+    @ResponseBody
+    public String criaLivro() {
+        Livro livro = new Livro("Livro " + Math.random());
+
+        repository.save(livro);
+
+       return "Foi: " + livro;
+    }
 
     @GetMapping("/")
     public String listaDeLivros(Model model) {
--- app/src/main/java/com/thoughtworks/aceleradora/biblioteca/Livro.java
+++ app/src/main/java/com/thoughtworks/aceleradora/biblioteca/Livro.java
@@ -1,6 +1,17 @@
 package com.thoughtworks.aceleradora.biblioteca;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
 public class Livro {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
 
     private String titulo;
 
